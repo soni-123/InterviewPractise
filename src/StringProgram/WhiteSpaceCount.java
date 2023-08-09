6d3f174a36ff16d28959ae44ed76de065be1ed33
soni-123 <rohitsoni912266@gmail.com>
2023-08-09 10:33:05 +0530

    Using isWhiteSpace function to get count

--- src/StringProgram/WhiteSpaceCount.java
+++ src/StringProgram/WhiteSpaceCount.java
@@ -5,11 +5,11 @@ public class WhiteSpaceCount {
 	public static void main(String[] args) {
 		int count=0;
 		String str="Selenium new version 4.11 which support chrome version 115";
-		String str1=str.replaceAll(" ", "@");
-		char ch[]=str1.toCharArray();
+		//String str1=str.replaceAll(" ", "@");// using replace all method we can do that 
+		char ch[]=str.toCharArray();
 		for(int i=0;i<ch.length;i++)
 		{
-			if(ch[i]=='@')
+			if(Character.isWhitespace(ch[i])) //Either we have whitespace function to get count
 			{
 				count++;
 			}
